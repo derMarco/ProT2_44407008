21a9621cd633c4ab8cad4111f64da606b4482a2c
derMarco <126200358+derMarco@users.noreply.github.com>
2024-06-21 23:25:48 -0300

    Add files via upload

---
+++ ProT2_44407008/front/login.php
@@ -0,0 +1,21 @@
+<!-- login-->
+<form>
+    <div class="container-fluid">
+        <h1 class="parrafo">Iniciar Sesión</h1>
+    </div>
+  <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3">
+    </div>
+  </div>
+  <div class="row mb-3">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3">
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Sign in</button>
+  <button class="btn btn-primary" type="reset" value="Reset">Reset</button>
+</form>
+<!-- fin login -->
---
+++ ProT2_44407008/front/registro.php
@@ -0,0 +1,29 @@
+<!-- formulario de registro -->
+        <form>
+            <div class="container-fluid">
+                <h1 class="parrafo">Registrarse</h1>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputName" class="form-label">Nombre</label><br>
+                <input type="name" class="form-control" id="exampleInputName" aria-describedby="nameHelp">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputSurname" class="form-label">Apellido</label><br>
+                <input type="surname" class="form-control" id="exampleInputSurname" aria-describedby="surnameHelp">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email</label><br>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputUser" class="form-label">Usuario</label><br>
+                <input type="user" class="form-control" id="exampleInputUser" aria-describedby="userHelp">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label><br>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <button class="btn btn-primary" type="reset" value="Reset">Reset</button>
+        </form> 
+<!-- fin de formulario -->

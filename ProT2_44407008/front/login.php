4bbf34d7a158948e484d1014fe66eb9421b250a0
derMarco <126200358+derMarco@users.noreply.github.com>
2024-06-23 15:46:51 -0300

    Update login.php
    se mejoro el login

--- ProT2_44407008/front/login.php
+++ ProT2_44407008/front/login.php
@@ -1,21 +1,30 @@
-<!-- login-->
-<form>
-    <div class="container-fluid">
-        <h1 class="parrafo">Iniciar Sesión</h1>
-    </div>
-  <div class="row mb-3">
-    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-    <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail3">
-    </div>
-  </div>
-  <div class="row mb-3">
-    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword3">
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Sign in</button>
-  <button class="btn btn-primary" type="reset" value="Reset">Reset</button>
+<div class="container mt-5 mb-5 d-flex justify-content-center">
+<div class="card" style="width: 50%;">
+<div class="card-header text-center">
+<h2>Iniciar sesion</h2>
+</div>
+<!-- Mensaje de Error -->
+<?php if(session()->getFlashdata('msg')):?>
+<div class="alert alert-warning">
+<?=session()->getFlashdata('msg')?>
+</div>
+<?php endif;?>
+<!-- Incio del formulario de login -->
+<form method="post" action="<?php echo base_url('/enviarlogin' ) ?>">
+<div class="card-body"media="(max-width:768px)">
+<div class="mb-2">
+<label for="exampleFormControlInput1" class="form-label">Correo</label>
+<input name="email" type="text" class="form-control" placeholder="Correo">
+</div>
+<div class="mb-3">
+<label for="exampleFormControlInput1" class="form-label">Password</label>
+<input name="pass" type="password" class="form-control" placeholder="contraseña">
+</div>
+<input type="submit" value="Ingresar" class="btn btn-success">
+<a href=" <?php echo base_url('login' ); ?>" class="btn btn-danger">Cancelar</a>
+<br><span>¿Aún no se registró? <a href="<?php echo base_url('/registrarse'); ?>">Registrarse aquí</a></span>
+</div>
 </form>
-<!-- fin login -->
+</div>
+</div>
+

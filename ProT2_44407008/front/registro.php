c3118ef22d1c1a596dab57d56a47767f33732680
derMarco <126200358+derMarco@users.noreply.github.com>
2024-06-23 15:50:07 -0300

    Update registro.php
    se mejoro el registro

--- ProT2_44407008/front/registro.php
+++ ProT2_44407008/front/registro.php
@@ -1,29 +1,76 @@
 <!-- formulario de registro -->
-        <form>
-            <div class="container-fluid">
-                <h1 class="parrafo">Registrarse</h1>
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputName" class="form-label">Nombre</label><br>
-                <input type="name" class="form-control" id="exampleInputName" aria-describedby="nameHelp">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputSurname" class="form-label">Apellido</label><br>
-                <input type="surname" class="form-control" id="exampleInputSurname" aria-describedby="surnameHelp">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email</label><br>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputUser" class="form-label">Usuario</label><br>
-                <input type="user" class="form-control" id="exampleInputUser" aria-describedby="userHelp">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label><br>
-                <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-            <button class="btn btn-primary" type="reset" value="Reset">Reset</button>
-        </form> 
+<div class="container mt-0 mb-0">
+<div class= "card-header text-justify">
+<div class="row d-flex justify-content-center">
+<div class="card col-lg-3" style="width: 50%;">
+<h4>Registrarse</h4>
+<?php $validation = \Config\Services:: validation(); ?>
+<form method="post" action="<? php echo base_url('/enviar-form') ?>">
+<?=csrf_field();?>
+<?php if(!empty (session()->getFlashdata('fail'))):?>
+<div class="alert alert-danger"> <?= session()->getFlashdata('fail');?></div>
+<?php endif?>
+<?php if(!empty (session()->getFlashdata('success'))):?>
+<div class="alert alert-danger"> <?= session()->getFlashdata('success');?></div>
+<?php endif?>
+<div class ="card-body justify-content-center" media="(max-width:768px)">
+<div class="form">
+<label for="exampleFormControlInput1"class="form-label">Nombre</label>
+<input name="nombre" type="text" class="form-control" placeholder="nombre" >
+<!-- Error -->
+<?php if($validation->getError('nombre')) {?>
+<div class="alert alert-danger mt-2">
+<?= $error=$validation->getError('nombre'); ?>
+</div>
+<?php }?>
+</div>
+<div class="mb-3">
+<label for="exampleFormControlTextareal" class="form-label">Apellido</label>
+<input type="text" name="apellido"class="form-control" placeholder="apellido" >
+<!-- Error -->
+<?php if($validation->getError('apellido')) {?>
+<div class='alert alert-danger mt-2'>
+<?= $error = $validation->getError('apellido'); ?>
+</div>
+<?php }?>
+</div>
+<div class="mb-3">
+<label for="exampleFormControlInput1" class="form-label">email</label>
+<input name="email" type="femail" class="form-control">
+<!-- Error -->
+<?php if($validation->getError('email')) {?>
+<div class='alert alert-danger mt-2'>
+<?= $error = $validation->getError('email'); ?>
+</div>
+<?php }?>
+</div>
+<div class="mb-3">
+<label for="exampleFormControlInput1" class="form-label">Usuario</label>
+<input tyupe="text" name="usuario" class="form-control" placeholder="usuario">
+<!-- Error -->
+<?php if($validation->getError('usuario')) {?>
+<div class="alert alert-danger mt-2">
+<?= $error = $validation->getError('usuario'); ?>
+</div>
+<?php }?>
+</div>
+<div class="mb-3">
+<div class="mb-3">
+<label for="exampleFormControlInput1" class="form-label">Password</label>
+<input name="pass" type="password" class="form-control" placeholder="password">
+<!-- Error -->
+<?php if($validation->getError('pass')) {?>
+<div class='alert alert-danger mt-2'>
+<?= $error = $validation->getError('pass'); ?>
+</div>
+<?php }?>
+</div>
+<input type="submit" value="guardar" class="btn btn-success">
+<input type="reset" value="cancelar" class="btn btn-danger">
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
 <!-- fin de formulario -->
